79551137231431ace86804d00ab387a4d4e8bcbd
PeterVenhuizen <peter_venhuizen@hotmail.com>
2016-02-09 10:25:47 +0100

    Update footer + header

--- footer.php
+++ footer.php
@@ -1,15 +1,7 @@
 		<div class="spacer"></div>
         <footer id="page_footer">
-            
+            <nav>
             <?php
-                
-                try {
-                	$stmt = $db->prepare("SELECT tag, color FROM tags");
-                	$stmt->execute();
-                	if ($stmt->rowCount() > 0) {
-                		foreach ($stmt as $row) { echo '<a class="tag" style="background-color:' . $row['color'] . ';" href="view.php?cat=' . str_replace(' ', '_', $key) . '">' . $row['tag'] . '</a>'; }
-                	}
-                } catch (PDOException $ex) { die(); }
                 
                 # Get all tags
                 $tag_count = array();
@@ -37,14 +29,18 @@
 
                 # Output tags
                 foreach ($tag_count as $k => $v) {
+                	echo '	<a class="tag" href="view.php?cat=' . str_replace(' ', '_', $k) . '" style="background-color: ' . $v[0] . ';">'. $k . ' ' . $v[1] . '</a>';
+                
+                	/*
                     echo '  <div class="tag_overview">
                                 <h3 style="background-color: ' . $v[0] . ';">' . $k . '</h3>
                                 <div class="tag_group">';
                     for ($x = 0; $x < $v[1]; $x++) { echo '<div class="square" style="background-color: ' . $v[0] . ';"></div>'; }
                     echo '      </div>
                             </div>';
+                    */
                 }
 
             ?>
-            
+            </nav>
 		</footer>
--- header.php
+++ header.php
@@ -1,15 +1,18 @@
         <?php include 'functions.php'; ?>
         <!-- http://www.bitrepository.com/php-autologin.html -->
         <header id="page_header" style="background-color: <?php echo $colors[array_rand($colors)]; ?>;">
+        	<style>
+        	<?php
+        		if (!isset($_SESSION['user'])) { echo "#a_logout { display: none; }"; }
+        		else { echo "#a_login { display: none; }"; }
+        	?>
+        	</style>
             <nav>
             	<a href="index.php"><img src="assets/img/menu_icon.png"></a>
                 <a href="add_post.php"><img src="assets/img/add_icon.png"></a>
                 <a href="list.php"><img src="assets/img/list_icon.png"></a>
-                <a href="login.php"><img src="assets/img/login_icon.png"></a>
-            	<ul>
-            		<li><a href="add_post.php">Add post</a></li>
-            		<li><a href="list.php">List</a></li>
-            	</ul>
+                <a href="login.php" id="a_login"><img src="assets/img/login_icon.png"></a>
+				<a href="logout.php" id="a_logout"><img src="assets/img/logout_icon.png"></a>
             <form id="search" method="POST" action="search.php">
             	<input type="search" id="search_field" name="search" placeholder="Search...">
             </form>
