6c4d5862a9be89855958e4a60a55dc9abdba87d9
Peter Venhuizen <petervenhuizen@MacBook-Air-3.local>
2016-02-09 20:35:31 +0100

    Restored file

--- upload.php
+++ upload.php
@@ -1,22 +1,22 @@
 <?php
-    require_once('../config.php');
-    require('../../functions.php');
+	$path = "images/";
 
-	if(!isset($_SESSION['user'])) { 
-		echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL=login.php">';
-		die();
+	if (file_exists($path . $_FILES["upload"]["name"])) {
+		echo $_FILES["upload"]["name"] . " already exists.";
 	} else {
-
-		if (isset($_POST['id'])) {
-
-            # Remove post
-		    try {
-		        $stmt = $db->prepare("DELETE FROM glossary WHERE id = :id");
-                $stmt->bindValue('id', $_POST['id']);
-		        $stmt->execute();
-		    } catch (PDOException $ex) { $ex->getMessage(); }
-			
-            remove_lonely_tags($db);
-		}
+		move_uploaded_file($_FILES["upload"]["tmp_name"], $path . $_FILES["upload"]["name"]);
+		// Required: anonymous function reference number as explained above.
+		$funcNum = $_GET['CKEditorFuncNum'] ;
+		// Optional: instance name (might be used to load a specific configuration file or anything else).
+		$CKEditor = $_GET['CKEditor'] ;
+		// Optional: might be used to provide localized messages.
+		$langCode = $_GET['langCode'] ;
+		 
+		// Check the $_FILES array and save the file. Assign the correct path to a variable ($url).
+		$url = $path . $_FILES["upload"]["name"];
+		// Usually you will only assign something here if the file could not be uploaded.
+		$message = '';
+		 
+		echo "<script type='text/javascript'> window.parent.CKEDITOR.tools.callFunction($funcNum, '$url', '$message');</script>";
 	}
 ?>
